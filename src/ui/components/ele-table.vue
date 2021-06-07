6ff3581d0ee1fb35e76d77fdbfac63109692f751
xuchunpeng <823736388@qq.com>
2021-06-07 09:09:02 +0800

    ele-table.vue修改

--- src/ui/components/ele-table.vue
+++ src/ui/components/ele-table.vue
@@ -1,22 +1,22 @@
 <template>
-    <el-table ref="ref_table" :data="tableData" style="width: 100%" :max-height="maxHeight" :key="componentKey" border stripe>
-        <el-table-column v-for="(col,idx) in columnConfig" :key="idx" :label="col.label" :width="col.width" :align="col.align">
-            <template slot-scope="scope">
-                <!-- <div  v-if="col.display"></div> -->
+    <div class="eleTableBox">
+        <el-table ref="ref_table" :data="tableData" style="width:calc(100% + 8px)" :height="maxHeight" :key="componentKey" border stripe>
+            <el-table-column v-for="(col,idx) in columnConfig" :key="idx" :label="col.label" :width="col.width" :align="col.align?col.align:'center'">
+                <template slot-scope="scope">
+                    <template v-if="col.type=='index'">{{scope.$index+1}}</template>
+                    <template v-else-if="col.type=='operate'">
+                        <span v-for="(item,index) in col.operates" :key="index" class="tableButton" style="margin:0 5px;" :style="getStyle(item)" v-html="item.display(scope)" @click="item.click?item.click(scope):''"></span>
+                    </template>
+                    <template v-else>
+                        <div v-if="col.display" v-html="col.display(scope)" :class="getClassname(col)" :style="getStyle(col)" @click="col.click?col.click(scope):''"></div>
+                        <div v-else :class="getClassname(col)" :style="getStyle(col)" @click="col.click?col.click(scope):''">{{scope.row[col.key]}}</div>
+                    </template>
 
-                <template v-if="col.type=='index'">{{scope.$index+1}}</template>
-                <template v-else-if="col.type=='button'">
-                    <div v-if="col.display" v-html="col.display(scope)" @click="col.click(scope)" class="tableButton"></div>
-                    <div v-else @click="col.click(scope)" class="tableButton">{{scope.row[col.key]}}</div>
                 </template>
+            </el-table-column>
+        </el-table>
+    </div>
 
-                <template v-else>
-                    <div v-if="col.display" v-html="col.display(scope)"></div>
-                    <div v-else>{{scope.row[col.key]}}</div>
-                </template>
-            </template>
-        </el-table-column>
-    </el-table>
 </template>
 
 <script>
@@ -32,6 +32,26 @@ export default {
             },
         },
     },
+    computed: {
+        getClassname: function () {
+            return (col) => {
+                let name = ''
+                if (col.type == 'button') {
+                    name += ' tableButton'
+                }
+                return name
+            }
+        },
+        getStyle: function () {
+            return (col) => {
+                let style = {}
+                if (col.style) {
+                    style = col.style
+                }
+                return style
+            }
+        },
+    },
     data() {
         return {
             maxHeight: '100%',
@@ -39,9 +59,16 @@ export default {
         }
     },
     mounted() {
-        this.maxHeight = this.$refs.ref_table.$el.parentNode.clientHeight
-        this.componentKey++
+        // this.maxHeight = this.$refs.ref_table.$el.parentNode.clientHeight
+        // this.componentKey++
     },
     methods: {},
 }
 </script>
+<style scoped lang="scss">
+.eleTableBox {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+}
+</style>
