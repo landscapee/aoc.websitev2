88d4076b8c3dd94aca4bf11df7a0ced721cbab13
xuchunpeng <823736388@qq.com>
2021-06-23 09:14:03 +0800

    格式

--- src/worker/process/tf-init.js
+++ src/worker/process/tf-init.js
@@ -55,27 +55,6 @@ postal.subscribe({
     topic: 'init',
     callback: (data) => {
 
-    let posWorker = myPostal('Worker');
-    let mySockets = socket(data.servers);
-    let httpRequest = new HttpRequest(data.httpConfig);
-    flightInit(posWorker, httpRequest);
-    monitorInit(posWorker, httpRequest);
-    MonitorWithRunwayInit(posWorker, httpRequest);
-    resourceMonitorInit(posWorker, httpRequest);
-    getSysConfigHttp(posWorker, httpRequest);
-    flightHttp(posWorker, httpRequest);
-    homeInit(posWorker, httpRequest)
-    delaysInit(posWorker, httpRequest)
-    memoryStore.setItem('global',{token:data.token});
-    postal.subscribe({
-      channel: 'Worker',
-      topic: 'LoginSuccess',
-      callback: (user)=>{
-        //登录成功
-        // 根据权限过滤航班
-        let roleData = find(user.roles, (item) => item.code.indexOf('DATA') > -1);
-        let roleFlights = get(roleData, 'menus.0.path');
-        roleFlights = roleFlights ? JSON.parse(roleFlights)[0] : { reversal: true, data: [] };
         let posWorker = myPostal('Worker');
         let mySockets = socket(data.servers);
         let httpRequest = new HttpRequest(data.httpConfig);
@@ -83,11 +62,11 @@ postal.subscribe({
         monitorInit(posWorker, httpRequest);
         MonitorWithRunwayInit(posWorker, httpRequest);
         resourceMonitorInit(posWorker, httpRequest);
+        getSysConfigHttp(posWorker, httpRequest);
         flightHttp(posWorker, httpRequest);
         homeInit(posWorker, httpRequest)
         delaysInit(posWorker, httpRequest)
-        TOBTConfigInit(posWorker, httpRequest)
-        memoryStore.setItem('global', {token: data.token});
+        memoryStore.setItem('global', { token: data.token });
         postal.subscribe({
             channel: 'Worker',
             topic: 'LoginSuccess',
@@ -96,13 +75,36 @@ postal.subscribe({
                 // 根据权限过滤航班
                 let roleData = find(user.roles, (item) => item.code.indexOf('DATA') > -1);
                 let roleFlights = get(roleData, 'menus.0.path');
-                roleFlights = roleFlights ? JSON.parse(roleFlights)[0] : {reversal: true, data: []};
+                roleFlights = roleFlights ? JSON.parse(roleFlights)[0] : { reversal: true, data: [] };
+                let posWorker = myPostal('Worker');
+                let mySockets = socket(data.servers);
+                let httpRequest = new HttpRequest(data.httpConfig);
+                flightInit(posWorker, httpRequest);
+                monitorInit(posWorker, httpRequest);
+                MonitorWithRunwayInit(posWorker, httpRequest);
+                resourceMonitorInit(posWorker, httpRequest);
+                flightHttp(posWorker, httpRequest);
+                homeInit(posWorker, httpRequest)
+                delaysInit(posWorker, httpRequest)
+                TOBTConfigInit(posWorker, httpRequest)
+                memoryStore.setItem('global', { token: data.token });
+                postal.subscribe({
+                    channel: 'Worker',
+                    topic: 'LoginSuccess',
+                    callback: (user) => {
+                        //登录成功
+                        // 根据权限过滤航班
+                        let roleData = find(user.roles, (item) => item.code.indexOf('DATA') > -1);
+                        let roleFlights = get(roleData, 'menus.0.path');
+                        roleFlights = roleFlights ? JSON.parse(roleFlights)[0] : { reversal: true, data: [] };
 
-                memoryStore.setItem('global', {
-                    token: data.token,
-                    roleFlights,
-                });
+                        memoryStore.setItem('global', {
+                            token: data.token,
+                            roleFlights,
+                        });
+                    }
+                })
             }
         })
     }
-});
+})
