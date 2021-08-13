692df8174f1aa60f9dc5d556add0dbd66ad6e27f
liuping <271578727@qq.com>
2021-08-13 14:15:33 +0800

    bug

--- src/ui/lib/transTimeByKey.js
+++ src/ui/lib/transTimeByKey.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 export const tranAtaAtd=(row)=>{
     let movement = get(row, 'movement');
     let time= movement === 'D'?row.atd:row.ata
-    return time?moment(time).format('HH:mm'):'--'
+    return time?moment(time).format('HH:mm')+`(${moment(time).format('DD')})`:'--'
 }
 export const tranStaStd=(row)=>{
     let movement = get(row, 'movement');
